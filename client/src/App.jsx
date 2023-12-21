db2c58485dc5be5c0af1f68c178fad1e4e182936
KEVINRIMACHI <rimachi.kevin7@gmail.com>
2023-12-21 12:51:22 -0500

    Primer commit

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+
+const URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000";
+
+function App() {
+  const [result, setResult] = useState('')
+
+  return (
+      <div>
+      <h1>MERN render</h1>
+      <button onClick={async()=>{
+        const res = await fetch(`${URL}/ping`)
+        const data = await res.json()
+        console.log(data);
+        setResult(data)
+      }}>
+        Users
+      </button>
+      <pre>{JSON.stringify(result, null, 2)}</pre>
+      </div>
+  )
+}
+
+export default App
